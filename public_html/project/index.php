1ccbe8a57e55040fdbb3ebee67558a8c06899e48
sarmedkhan2099 <sarmedkhan2099@gmail.com>
2025-08-04 23:52:03 -0400

    Update index.php

---
+++ public_html/project/index.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+if (isset($_SESSION['username'])) {
+    header("Location: Dashboard.php");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Film Manager - Home</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Welcome to the Plumber Management System</h1>
+    <p>Please <a href="Login.php">Login</a> or <a href="Register.php">Register</a> to continue.</p>
+</body>
+</html>
